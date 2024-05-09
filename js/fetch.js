b9542b107bdfd9b6dab19dfda2c534bbacb1e4b8
serlip06 <114634943+serlip06@users.noreply.github.com>
2024-05-09 17:25:49 +0700

    pushh lagiii

--- js/fetch.js
+++ js/fetch.js
@@ -1,7 +1,7 @@
 import { get } from "https://bukulapak.github.io/api/process.js"; 
 let urlAPI = "https://ats-714220023-serlipariela-38bba14820aa.herokuapp.com/pelanggan";
-get(urlAPI,isiTablePresensi);
+get(urlAPI,GetAllPelanggan);
 
-function isiTablePresensi(results){
+function GetAllPelanggan(results){
     console.log(results);
 }
