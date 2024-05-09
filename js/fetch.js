482a3410c2cc8bac47422bde858304e8d3fd37ba
serlip06 <114634943+serlip06@users.noreply.github.com>
2024-05-09 16:57:56 +0700

    semoga berhasilll!!!

--- js/fetch.js
+++ js/fetch.js
@@ -1,5 +1,5 @@
 import { get } from "https://bukulapak.github.io/api/process.js"; 
-let urlAPI = "https://ats-714220023-serlipariela-38bba14820aa.herokuapp.com/";
+let urlAPI = "https://ats-714220023-serlipariela-38bba14820aa.herokuapp.com/pelanggan";
 get(urlAPI,isiTablePresensi);
 
 function isiTablePresensi(results){
