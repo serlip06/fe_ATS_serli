cc1e1199ba61299badf7d167613cbee04f9f48a5
serlip06 <114634943+serlip06@users.noreply.github.com>
2024-05-09 21:34:43 +0700

    pushh

--- js/fetch.js
+++ js/fetch.js
@@ -1,7 +1,13 @@
 import { get } from "https://bukulapak.github.io/api/process.js";
+import { setInner } from "https://bukulapak.github.io/element/process.js";
 let urlAPI = "https://ats-714220023-serlipariela-38bba14820aa.herokuapp.com/pelanggan";
 get(urlAPI,GetAllPelanggan);
 
 function GetAllPelanggan(results){
     console.log(results);
+    results.forEach(isiRow);
+} 
+
+function isiRow(value){
+    console.log(value)
 }
