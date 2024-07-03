8133da4a72e8cbd348fc1f91c385c81478e60d02
qintharganteng <qintharalmaliki@gmail.com>
2024-07-03 13:00:53 +0700

    benerin

--- js/temp/table.js
+++ js/temp/table.js
@@ -17,10 +17,6 @@ export let isiTabel =
                             <td class="px-6 py-4 whitespace-no-wrap bg-white  text-sm font-medium coolGray-500">
                                 <a href="#">#EMAIL#</a>
                             </td>
-                            <td class="px-6 py-4 whitespace-no-wrap bg-white text-sm font-medium text-coolGray-500">#NAMA_PRODUK#</td>
-                              <td class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-500 text-left">#DESKRIPSI#</td>
-                            <td class="px-6 py-4 whitespace-nowrap  bg-white text-sm font-medium text-coolGray-500">#HARGA#</td>
-                            <td class="px-6 py-4 whitespace-nowrap  bg-white text-sm font-medium text-coolGray-500">#STOK#</td>
                            <td class="whitespace-nowrap pr-4 bg-white text-sm font-medium text-coolGray-800">
                             <a class="inline-block px-3 py-1 bg-yellow-500 text-white rounded-lg cursor-pointer shadow-md active:scale-95 transition-transform" href="#">Edit</a>
                             <span class="mx-1">|</span>
