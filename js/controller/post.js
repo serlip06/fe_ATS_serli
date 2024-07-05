99942b8da1380721dedcb80dcaef1e460df3e332
qintharganteng <qintharalmaliki@gmail.com>
2024-07-05 10:51:34 +0700

    insert trus bos

--- js/controller/post.js
+++ js/controller/post.js
@@ -2,25 +2,37 @@ import { postData } from "https://bukulapak.github.io/api/process.js";
 import { onClick, getValue } from "https://bukulapak.github.io/element/process.js";
 import { urlPOST, AmbilResponse} from "../config/url_post.js";
 
+  function pushData() {
+    const fullName = getValue("full_name");
+    const phoneNumber = getValue("phone_number");
 
-function pushData(){
-    
-    var stok = getValue("stok");
+    if (/[^a-zA-Z\s]/.test(fullName)) {
+      document.getElementById('full_name_error_message').style.display = 'flex';
+      return;
+    } else {
+      document.getElementById('full_name_error_message').style.display = 'none';
+    }
+
+    if (/[^0-9]/.test(phoneNumber)) {
+      document.getElementById('phone_number_error_message').style.display = 'flex';
+      return;
+    } else {
+      document.getElementById('phone_number_error_message').style.display = 'none';
+    }
 
     let data = {
-        nama : getValue(getValue("nama")),
-        phone_number : getValue(getValue("phone_number")),
-        alamat : getValue("alamat"),
-        phone_number : getValue("phone_number"),
-        email : getValue("email"),
-        nama_produk : getValue("nama_produk"),
-        deskripsi : getValue("deskripsi"),
-        harga : getValue("harga"),
-        gambar : getValue("gambar"),
-        stok : stok,
-    }
-    postData(urlPOST, data, AmbilResponse);
+      nama: fullName,
+      phone_number: phoneNumber,
+      alamat: getValue("alamat"),
+      email: getValue("email")
+    };
 
-}
+    postData('https://ats-714220023-serlipariela-38bba14820aa.herokuapp.com/insert',urlPOST, data, AmbilResponse);
+  }
 
-onClick("button", pushData);
+  document.getElementById('submit_button').addEventListener('click', function(event) {
+    event.preventDefault();
+    pushData();
+  });
+
+  onClick("button", pushData)
