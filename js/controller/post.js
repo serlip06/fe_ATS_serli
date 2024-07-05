b48f63a453fbcda6ad3b64620b15320680c881f8
qintharganteng <qintharalmaliki@gmail.com>
2024-07-05 11:02:50 +0700

    insertttt

--- js/controller/post.js
+++ js/controller/post.js
@@ -1,38 +1,38 @@
 import { postData } from "https://bukulapak.github.io/api/process.js";
 import { onClick, getValue } from "https://bukulapak.github.io/element/process.js";
-import { urlPOST, AmbilResponse} from "../config/url_post.js";
+import { urlPOST, AmbilResponse } from "../config/url_post.js";
 
-  function pushData() {
+function pushData() {
     const fullName = getValue("full_name");
     const phoneNumber = getValue("phone_number");
 
     if (/[^a-zA-Z\s]/.test(fullName)) {
-      document.getElementById('full_name_error_message').style.display = 'flex';
-      return;
+        document.getElementById('full_name_error_message').style.display = 'flex';
+        return;
     } else {
-      document.getElementById('full_name_error_message').style.display = 'none';
+        document.getElementById('full_name_error_message').style.display = 'none';
     }
 
     if (/[^0-9]/.test(phoneNumber)) {
-      document.getElementById('phone_number_error_message').style.display = 'flex';
-      return;
+        document.getElementById('phone_number_error_message').style.display = 'flex';
+        return;
     } else {
-      document.getElementById('phone_number_error_message').style.display = 'none';
+        document.getElementById('phone_number_error_message').style.display = 'none';
     }
 
     let data = {
-      nama: fullName,
-      phone_number: phoneNumber,
-      alamat: getValue("alamat"),
-      email: getValue("email")
+        nama: fullName,
+        phone_number: phoneNumber,
+        alamat: getValue("alamat"),
+        email: getValue("email")
     };
 
-    postData('https://ats-714220023-serlipariela-38bba14820aa.herokuapp.com/insert',urlPOST, data, AmbilResponse);
-  }
+    postData('https://ats-714220023-serlipariela-38bba14820aa.herokuapp.com/insert', data, AmbilResponse);
+}
 
-  document.getElementById('submit_button').addEventListener('click', function(event) {
+document.getElementById('submit_button').addEventListener('click', function(event) {
     event.preventDefault();
     pushData();
-  });
+});
 
-  onClick("button", pushData)
+onClick("button", pushData);
