79efcf45049019f81534b1d183cf61d33a19ebf6
serlip06 <114634943+serlip06@users.noreply.github.com>
2024-07-11 13:19:05 +0700

    push

--- js/config/url_get_detail.js
+++ js/config/url_get_detail.js
@@ -2,4 +2,5 @@
 const urlParams = new URLSearchParams(window.location.search);
 const customerId = urlParams.get("customerId");
 
+
 export let urlFetch = "https://ats-714220023-serlipariela-38bba14820aa.herokuapp.com/customer/" + customerId;
